95b301c69eca2ad4a34a6ae0d535b491baa1a05e
lenorcon <lenorcon458@gmail.com>
2023-12-27 13:14:16 +0800

    this is my commit

--- vue-chat-app/public/server.js
+++ vue-chat-app/public/server.js
@@ -1,12 +1,72 @@
 const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
 const app = express();
+const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'secret', resave: true, saveUninitialized: true }));
 
-// Your other route definitions
-// ...
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: '',
+    database: 'chat_app'
 });
+
+db.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL database');
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the registration page!');
+});
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = { username, email, password: hashedPassword };
+
+    db.query('INSERT INTO users SET ?', user, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.send('Registration failed.');
+        } else {
+            console.log(results);
+            res.send('Registration successful.');
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = { username,  password: hashedPassword };
+
+    db.query('INSERT INTO users SET ?', user, (err, results) => {
+        if (err) {
+            console.log(err);
+            res.send('login failed.');
+        } else {
+            console.log(results);
+            res.send('login successful.');
+        }
+    });
+});
+
+
